ac4b03b52150a3ee2139b41e1dd134ad461825fb
jimportico <30626644+jimportico@users.noreply.github.com>
2020-07-20 14:28:43 -0400

    Add files via upload

--- ml prototypes/hyperparameter optimization.py
+++ ml prototypes/hyperparameter optimization.py
@@ -1,7 +1,6 @@
 """
 Creating a script to test different approaches to optimize hyperparameters
     video: https://www.youtube.com/watch?v=5nYqK-HaoKY
-    Pick up at 38 minutes
 """
 
 import os
@@ -16,6 +15,7 @@ from sklearn import model_selection
 from sklearn import decomposition
 from sklearn import preprocessing
 from sklearn import pipeline
+import sklearn
 from skopt.space.space import Categorical
 
 # List directory with data
@@ -89,7 +89,7 @@ print(random_model.best_estimator_.get_params())
 
 
 # Bayesian Search - skopt
-# ---------------
+# -----------------------
 from functools import partial
 from skopt import space
 from skopt import gp_minimize
@@ -136,4 +136,53 @@ result = gp_minimize(
     verbose=10,
 )
 
-print(dict(param_names, result.x))
+print(dict(zip(param_names, result.x)))
+
+# Hyperopt
+# --------
+from hyperopt import hp, fmin, tpe, Trials
+from hyperopt.pyll import scope
+
+# create a function
+def hyp_optimize(params, x, y):
+    """Optimization function for bayesian search"""
+    model = ensemble.RandomForestClassifier(**params)
+    kf = model_selection.StratifiedKFold(n_splits=5)
+    accuracies = []
+    for idx in kf.split(x, y):
+        train_idx, test_idx = idx[0], idx[1]
+        xtrain = x[train_idx]
+        ytrain = y[train_idx]
+
+        xtest = x[test_idx]
+        ytest = y[test_idx]
+
+        model.fit(xtrain, ytrain)
+        preds = model.predict(xtest)
+        fold_acc = metrics.accuracy_score(ytest, preds)
+        accuracies.append(fold_acc)
+
+    return -1 * np.mean(accuracies)
+
+
+hyp_param_space = {
+    "max_depth": scope.int(hp.quniform("max_depth", 3, 15, 1)),
+    "n_estimators": scope.int(hp.quniform("n_estimators", 100, 600, 1)),
+    "criterion": hp.choice("criterion", ["gini", "entropy"]),
+    "max_features": hp.uniform("max_features", 0.01, 1),
+}
+
+hyp_optimization_function = partial(hyp_optimize, x=X, y=y)
+
+trials = Trials()
+
+result = fmin(
+    fn=hyp_optimization_function,
+    space=hyp_param_space,
+    max_evals=15,
+    trials=trials,
+    algo=tpe.suggest,
+)
+
+
+print(result)
